3947b205e8a3160f6a610df8dd51f6eae2178413
anhhuy0168 <anhhuy123zxc123@gmail.com>
2024-04-22 11:00:56 +0700

    Add Animation

--- yuno-app/src/components/Products/ViewProducts.jsx
+++ yuno-app/src/components/Products/ViewProducts.jsx
@@ -1,11 +1,35 @@
 import React, { useContext, useEffect, useState } from "react";
 import { ProductContext } from "../context/productContext";
 import { Link } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
 const ViewProducts = () => {
-  const { productState: { products }, getProduct } = useContext(ProductContext);
+  const {
+    productState: { products },
+    getProduct,
+  } = useContext(ProductContext);
   const [randomProducts1, setRandomProducts1] = useState([]);
   const [randomProducts2, setRandomProducts2] = useState([]);
+  AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: "aos-init", // class applied after initialization
+    animatedClassName: "aos-animate", // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: "ease", // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+  });
   useEffect(() => {
     // Tạo danh sách sản phẩm ngẫu nhiên
     const getRandomProducts = (arr, n) => {
@@ -25,29 +49,39 @@ const ViewProducts = () => {
         <div className="showcase-wrapper has-scrollbar">
           <div className="showcase-container">
             {randomProducts1.map((product, index) => (
-                     <Link to={`/productDetail/${product.id}`}>
-              <div className="showcase" key={index} style={{marginBottom:"10px"}}>
-                <a href="#" className="showcase-img-box">
-                  <img
-                    src={product.productImage}
-                    alt={product.productName}
-                    width={70}
-                    className="showcase-img"
-                  />
-                </a>
-                <div className="showcase-content">
-                  <a href="#">
-                    <h4 className="showcase-title">{product.productName}</h4>
+              <Link to={`/productDetail/${product.id}`}>
+                <div
+                  className="showcase"
+                  key={index}
+                  style={{ marginBottom: "10px" }}
+                  data-aos="fade-right"
+                  data-aos-delay={index * 100}
+                  data-aos-duration="500"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  data-aos-once="false"
+                >
+                  <a href="#" className="showcase-img-box">
+                    <img
+                      src={product.productImage}
+                      alt={product.productName}
+                      width={70}
+                      className="showcase-img"
+                    />
                   </a>
-                  <a href="#" className="showcase-category">
-                    {product.category}
-                  </a>
-                  <div className="price-box">
-                    <p className="price">${product.salePrice}.00</p>
-                    <del>${product.currentPrice}.00</del>
+                  <div className="showcase-content">
+                    <a href="#">
+                      <h4 className="showcase-title">{product.productName}</h4>
+                    </a>
+                    <a href="#" className="showcase-category">
+                      {product.category}
+                    </a>
+                    <div className="price-box">
+                      <p className="price">${product.salePrice}.00</p>
+                      <del>${product.currentPrice}.00</del>
+                    </div>
                   </div>
                 </div>
-              </div>
               </Link>
             ))}
           </div>
@@ -56,33 +90,43 @@ const ViewProducts = () => {
       <div className="product-showcase">
         <h2 className="title">Trending</h2>
         <div className="showcase-wrapper has-scrollbar">
-          <div className="showcase-container" >
+          <div className="showcase-container">
             {/* Hiển thị randomProducts2 */}
             {randomProducts2.map((product, index) => (
-                 <Link to={`/productDetail/${product.id}`}>
-                 <div className="showcase" key={index} style={{marginBottom:"10px"}}>
-                   <a href="#" className="showcase-img-box">
-                     <img
-                       src={product.productImage}
-                       alt={product.productName}
-                       width={70}
-                       className="showcase-img"
-                     />
-                   </a>
-                   <div className="showcase-content">
-                     <a href="#">
-                       <h4 className="showcase-title">{product.productName}</h4>
-                     </a>
-                     <a href="#" className="showcase-category">
-                       {product.category}
-                     </a>
-                     <div className="price-box">
-                       <p className="price">${product.salePrice}.00</p>
-                       <del>${product.currentPrice}.00</del>
-                     </div>
-                   </div>
-                 </div>
-                 </Link>
+              <Link to={`/productDetail/${product.id}`}>
+                <div
+                  className="showcase"
+                  key={index}
+                  style={{ marginBottom: "10px" }}
+                  data-aos="fade-left"
+                  data-aos-delay={index * 100}
+                  data-aos-duration="500"
+                  data-aos-easing="ease-in-out"
+                  data-aos-mirror="true"
+                  data-aos-once="false"
+                >
+                  <a href="#" className="showcase-img-box">
+                    <img
+                      src={product.productImage}
+                      alt={product.productName}
+                      width={70}
+                      className="showcase-img"
+                    />
+                  </a>
+                  <div className="showcase-content">
+                    <a href="#">
+                      <h4 className="showcase-title">{product.productName}</h4>
+                    </a>
+                    <a href="#" className="showcase-category">
+                      {product.category}
+                    </a>
+                    <div className="price-box">
+                      <p className="price">${product.salePrice}.00</p>
+                      <del>${product.currentPrice}.00</del>
+                    </div>
+                  </div>
+                </div>
+              </Link>
             ))}
           </div>
         </div>
--- yuno-app/src/components/layout/Blog.jsx
+++ yuno-app/src/components/layout/Blog.jsx
@@ -1,112 +1,139 @@
-import React from 'react'
-
+import React from "react";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
 const Blog = () => {
+  AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: "aos-init", // class applied after initialization
+    animatedClassName: "aos-animate", // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: "ease", // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+  });
   return (
     <main>
-            <div className="blog">
-      <div className="container">
-        <div className="blog-container has-scrollbar">
-          <div className="blog-card">
-            <a href="#">
-              <img
-                src="https://res.cloudinary.com/da3bmd8ak/image/upload/v1712736280/blog-1_ma5xwq.jpg"
-                alt="Clothes Retail KPIs 2021 Guide for Clothes Executives"
-                width={300}
-                className="blog-banner"
-              />
-            </a>
-            <div className="blog-content">
-              <a href="#" className="blog-category">
-                Fashion
-              </a>
+      <div
+        className="blog"
+        data-aos="fade-up"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out"
+      >
+        <div className="container">
+          <div className="blog-container has-scrollbar">
+            <div className="blog-card">
               <a href="#">
-                <h3 className="blog-title">
-                  Clothes Retail KPIs 2021 Guide for Clothes Executives.
+                <img
+                  src="https://res.cloudinary.com/da3bmd8ak/image/upload/v1712736280/blog-1_ma5xwq.jpg"
+                  alt="Clothes Retail KPIs 2021 Guide for Clothes Executives"
+                  width={300}
+                  className="blog-banner"
+                />
+              </a>
+              <div className="blog-content">
+                <a href="#" className="blog-category">
+                  Fashion
+                </a>
+                <a href="#">
+                  <h3 className="blog-title">
+                    Clothes Retail KPIs 2021 Guide for Clothes Executives.
+                  </h3>
+                </a>
+                <p className="blog-meta">
+                  By <cite>Mr Admin</cite> /{" "}
+                  <time dateTime="2022-04-06">Apr 06, 2024</time>
+                </p>
+              </div>
+            </div>
+            <div className="blog-card">
+              <a href="#">
+                <img
+                  src="https://res.cloudinary.com/da3bmd8ak/image/upload/v1712736279/blog-2_sjskwe.jpg"
+                  alt="Curbside fashion Trends: How to Win the Pickup Battle."
+                  className="blog-banner"
+                  width={300}
+                />
+              </a>
+              <div className="blog-content">
+                <a href="#" className="blog-category">
+                  Clothes
+                </a>
+                <h3>
+                  <a href="#" className="blog-title">
+                    Curbside fashion Trends: How to Win the Pickup Battle.
+                  </a>
                 </h3>
-              </a>
-              <p className="blog-meta">
-                By <cite>Mr Admin</cite> /{" "}
-                <time dateTime="2022-04-06">Apr 06, 2024</time>
-              </p>
+                <p className="blog-meta">
+                  By <cite>Mr Robin</cite> /{" "}
+                  <time dateTime="2022-01-18">Jan 18, 2024</time>
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="blog-card">
-            <a href="#">
-              <img
-                src="https://res.cloudinary.com/da3bmd8ak/image/upload/v1712736279/blog-2_sjskwe.jpg"
-                alt="Curbside fashion Trends: How to Win the Pickup Battle."
-                className="blog-banner"
-                width={300}
-              />
-            </a>
-            <div className="blog-content">
-              <a href="#" className="blog-category">
-                Clothes
+            <div className="blog-card">
+              <a href="#">
+                <img
+                  src="https://res.cloudinary.com/da3bmd8ak/image/upload/v1712736278/blog-3_imxomj.jpg"
+                  alt="EBT vendors: Claim Your Share of SNAP Online Revenue."
+                  className="blog-banner"
+                  width={300}
+                />
               </a>
-              <h3>
-                <a href="#" className="blog-title">
-                  Curbside fashion Trends: How to Win the Pickup Battle.
+              <div className="blog-content">
+                <a href="#" className="blog-category">
+                  Shoes
                 </a>
-              </h3>
-              <p className="blog-meta">
-                By <cite>Mr Robin</cite> /{" "}
-                <time dateTime="2022-01-18">Jan 18, 2024</time>
-              </p>
+                <h3>
+                  <a href="#" className="blog-title">
+                    EBT vendors: Claim Your Share of SNAP Online Revenue.
+                  </a>
+                </h3>
+                <p className="blog-meta">
+                  By <cite>Mr Selsa</cite> /{" "}
+                  <time dateTime="2022-02-10">Feb 10, 2024</time>
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="blog-card">
-            <a href="#">
-              <img
-                src="https://res.cloudinary.com/da3bmd8ak/image/upload/v1712736278/blog-3_imxomj.jpg"
-                alt="EBT vendors: Claim Your Share of SNAP Online Revenue."
-                className="blog-banner"
-                width={300}
-              />
-            </a>
-            <div className="blog-content">
-              <a href="#" className="blog-category">
-                Shoes
+            <div className="blog-card">
+              <a href="#">
+                <img
+                  src="https://res.cloudinary.com/da3bmd8ak/image/upload/v1712736279/blog-4_ui8mt0.jpg"
+                  alt="Curbside fashion Trends: How to Win the Pickup Battle."
+                  className="blog-banner"
+                  width={300}
+                />
               </a>
-              <h3>
-                <a href="#" className="blog-title">
-                  EBT vendors: Claim Your Share of SNAP Online Revenue.
+              <div className="blog-content">
+                <a href="#" className="blog-category">
+                  Electronics
                 </a>
-              </h3>
-              <p className="blog-meta">
-                By <cite>Mr Selsa</cite> /{" "}
-                <time dateTime="2022-02-10">Feb 10, 2024</time>
-              </p>
-            </div>
-          </div>
-          <div className="blog-card">
-            <a href="#">
-              <img
-                src="https://res.cloudinary.com/da3bmd8ak/image/upload/v1712736279/blog-4_ui8mt0.jpg"
-                alt="Curbside fashion Trends: How to Win the Pickup Battle."
-                className="blog-banner"
-                width={300}
-              />
-            </a>
-            <div className="blog-content">
-              <a href="#" className="blog-category">
-                Electronics
-              </a>
-              <h3>
-                <a href="#" className="blog-title">
-                  Curbside fashion Trends: How to Win the Pickup Battle.
-                </a>
-              </h3>
-              <p className="blog-meta">
-                By <cite>Mr Pawar</cite> /{" "}
-                <time dateTime="2022-03-15">Mar 15, 2024</time>
-              </p>
+                <h3>
+                  <a href="#" className="blog-title">
+                    Curbside fashion Trends: How to Win the Pickup Battle.
+                  </a>
+                </h3>
+                <p className="blog-meta">
+                  By <cite>Mr Pawar</cite> /{" "}
+                  <time dateTime="2022-03-15">Mar 15, 2024</time>
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </main>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
